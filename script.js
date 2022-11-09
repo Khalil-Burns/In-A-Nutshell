a6e646e54c1938cecbf73c11a35289e3e60aa233
KHALIL BURNS <52607127+Khalil-Burns@users.noreply.github.com>
2022-11-09 15:56:10 +0000

    lots of stuff

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-console.log("hello\n\n\n")
-console.log("test test test")
+var boldButton = document.getElementById("B");
+var italicsButton = document.getElementById("I");
+var underlineButton = document.getElementById("U");
+var askFieldInput = document.getElementById("askFieldInput");
+
+boldButton.addEventListener("click", function(){
+  // Toggle the bold class on the text element
+  askFieldInput.classList.toggle("bold");
+});
+italicsButton.addEventListener("click", function(){
+  // Toggle the bold class on the text element
+  askFieldInput.classList.toggle("italic");
+});
+underlineButton.addEventListener("click", function(){
+  // Toggle the bold class on the text element
+  askFieldInput.classList.toggle("underline");
+});
