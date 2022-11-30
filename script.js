e2780db1e369f51940974d46f38b8b2244199c35
KHALIL BURNS <52607127+Khalil-Burns@users.noreply.github.com>
2022-11-30 13:46:19 +0000

    updates

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var boldButton = document.getElementById("B");
+/*var boldButton = document.getElementById("B");
 var italicsButton = document.getElementById("I");
 var underlineButton = document.getElementById("U");
 var askFieldInput = document.getElementById("askFieldInput");
@@ -14,4 +14,9 @@ italicsButton.addEventListener("click", function(){
 underlineButton.addEventListener("click", function(){
   // Toggle the bold class on the text element
   askFieldInput.classList.toggle("underline");
-});
+});*/
+function go() {
+    var question =      tinymce.get('questionField').getContent();
+    tinymce.get('questionField').setContent('');
+    console.log(question);
+}
