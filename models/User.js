93abc890b944721c813d11f82a47af3038d955af
Khalil-Burns <khalilburns@gmail.com>
2022-12-02 09:25:25 -0500

    added some updates

---
+++ models/User.js
@@ -0,0 +1,13 @@
+class Question {
+    constructor(id, likes, dislikes, username, email, password, ) {
+        this.id = id;
+        this.likes = likes;
+        this.dislikes = dislikes;
+        this.wordCnt = wordCnt;
+        this.question = question;
+        this.tags = tags;
+        this.text = text;
+    }
+}
+
+module.exports = Question;
