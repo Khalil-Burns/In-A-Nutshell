72257b373243888515641feab2244de75b56f279
Khalil-Burns <khalilburns@gmail.com>
2022-12-01 20:05:12 -0500

    improve UI

--- public/script/script.js
+++ public/script/script.js
@@ -1,4 +1,5 @@
 /*var boldButton = document.getElementById("B");
+
 var italicsButton = document.getElementById("I");
 var underlineButton = document.getElementById("U");
 var askFieldInput = document.getElementById("askFieldInput");
@@ -14,25 +15,32 @@ italicsButton.addEventListener("click", function(){
 underlineButton.addEventListener("click", function(){
   // Toggle the bold class on the text element
   askFieldInput.classList.toggle("underline");
-});*/
+});
 
 
-// function go() {
-//   var xhttp = new XMLHttpRequest();
-//   var question = tinymce.get('questionField').getContent();
-//   var wordCnt = tinymce.activeEditor.plugins.wordcount.getCount();
-//   tinymce.get('questionField').setContent('');
+function go() {
+  var xhttp = new XMLHttpRequest();
+  var question = tinymce.get('questionField').getContent();
+  var wordCnt = tinymce.activeEditor.plugins.wordcount.getCount();
+  tinymce.get('questionField').setContent('');
 
-//   xhttp.open("POST", '/', true);
-//   xhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-//   /*xhttp.send(
-//     {
-//       'text': question, 
-//       'wordCnt': wordCnt
-//     });*/
-//   xhttp.send(`test=${question}&wordCnt=${wordCnt}`);
-//   console.log(question);
-// }
+  xhttp.open("POST", '/', true);
+  xhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+  /*xhttp.send(
+    {
+      'text': question, 
+      'wordCnt': wordCnt
+    });
+  xhttp.send(`test=${question}&wordCnt=${wordCnt}`);
+  console.log(question);
+}*/
+
+var closePopup = document.getElementsByClassName("close")[0];
+var submitQuestionPopup = document.getElementById("submitPopup");
+
+function dropdownOnClick() {
+  document.getElementById("userDropdown").classList.toggle("show");
+}
 
 function post() {
   var question = document.getElementById("title").value;
@@ -47,4 +55,30 @@ function post() {
     text: text,
     wordCnt: wordCnt
   });
+
+  submitQuestionPopup.style.display = "block";
 };
+
+closePopup.onclick = function() {
+  submitQuestionPopup.style.display = "none";
+  console.log('yes');
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropdownButton')) {
+    var dropdowns = document.getElementsByClassName("dropdownOnClick");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+window.onclick = function(event) {
+  if (event.target == submitQuestionPopup) {
+    submitQuestionPopup.style.display = "none";
+  }
+}
