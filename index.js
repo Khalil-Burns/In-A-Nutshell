0b2e832390287a66e1462a66b2598ada0382582a
Khalil-Burns <khalilburns@gmail.com>
2022-12-02 09:42:27 -0500

    asdf

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 
 app.set('view-engine', 'html');
 app.engine('html', require('ejs').renderFile);
-app.use(express.static(path.join(_dir, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
