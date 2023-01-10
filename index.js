5a9a65f4a4002f88f3690ef8950af5ec896fede4
VertiMat <matthewlim888888@gmail.com>
2023-01-10 09:24:26 -0500

    Okey

--- index.js
+++ index.js
@@ -70,6 +70,9 @@ app.get('/question/:id', async (req, res, next) => {
         res.send('Question not found!');
     }
 });
+app.get('/about', async (req, res, next) => {
+    res.render(`${__dirname}/about.html`);
+})
 
 app.post('/like/:id', async (req, res, next) => {
     await like(req, res, next);
