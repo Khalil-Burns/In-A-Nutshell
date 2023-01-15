2102254d0b78a529c9517dd9dd14fb8112842fad
Khalil-Burns <52607127+Khalil-Burns@users.noreply.github.com>
2023-01-14 23:27:08 -0500

    Update TagController.js

--- controllers/TagController.js
+++ controllers/TagController.js
@@ -1,7 +1,7 @@
 /*
  *  Authors: Khalil Burns and Matthew Lim
  *  
- *  File Description: read and write for notifications
+ *  File Description: read and write for tags
  *  
  *  Method Descriptions:
  *
