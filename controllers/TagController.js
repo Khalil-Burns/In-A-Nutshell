b5eba9c5e3a93baebc092426ae2d5844862aa78f
Khalil-Burns <52607127+Khalil-Burns@users.noreply.github.com>
2023-01-14 23:27:51 -0500

    Update TagController.js

--- controllers/TagController.js
+++ controllers/TagController.js
@@ -9,11 +9,13 @@
  *          req: request (all the request data)
  *          res: result (not used)
  *          next: next (not used)
+ *      return: array
  *
  *      addTags(id, text, img): add tag to the list of current tags (in the tags array in the tags collection)
  *          req: request (all the request data)
  *          res: result (not used)
  *          next: next (not used)
+ *      return: null
  */
  
 const firebase = require('../db');
